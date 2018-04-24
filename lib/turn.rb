f29b546d6fbcdfe19ad40f58e379fd6ccd39b7dd
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-24 03:33:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,13 +11,13 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
-  if index.between?(0, 8) && !position_taken?(board, index)
+  if index.between?(0, 8) && position_taken?(board, index)
     true 
   end
 end
 
 def position_taken?(board, index)
-  board[index] != ""
+  board[index] == ""
   false 
 end
 
