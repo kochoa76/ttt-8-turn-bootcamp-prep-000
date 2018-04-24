6e6d55fcdbe7b941ea2f7111725bd2fd0c68be1b
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-24 04:08:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,12 +28,20 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if index.valid_move?(board, index)
+  if valid_move?(board, index)
     move(board, index)
     display_board(board)
-  end 
-else turn(board)
+  else turn(board)
+  end
 end
+
+
+
+
+
+
+
+
 
 
 
