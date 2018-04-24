a2535a9a69016ce93da0dc219a0ba5981a2368bb
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-24 03:18:49 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,18 @@
-def display_board
+def display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+end
+
+def valid_move?(board, index)
+  if index.between?(1, 9) && position_taken?
+    true 
+  end
+end
+
+def position_taken?(board, index)
+  board[index] == ""
+  false 
 end
